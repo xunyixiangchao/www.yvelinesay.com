47ce6862ef645c79d7b4246eaf345a82ed74f3bf
yvln <sayyveline@gmail.com>
2017-10-31 14:57:45 -0400

    update projects

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -8,6 +8,7 @@ class Project extends Component {
       <img className="image" src={this.props.image} />
       <div className="ProjectText">
         <div className="title">{this.props.title}</div>
+        <div className="technology">Technology: {this.props.technology}</div>
         <div className="description">{this.props.description}</div>
         <div className="links">
           <a href={this.props.linkToGit} target="_blank"><img className="imgGit" src="./images/github.png" /></a>
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -10,35 +10,45 @@ class Projects extends Component {
         <div className="Projects">
 
           <Project
-            title="BattleShip"
-            description="Nanani"
-            linkToGit="https://git.generalassemb.ly/yvln-ga/project1-battleship"
-            linkToWeb="https://git.generalassemb.ly/pages/yvln-ga/project1-battleship/"
-            image="./images/battleship.png"
-          />
-
-          <Project
-            title="Remembook"
-            description="Nanani"
-            linkToGit="https://git.generalassemb.ly/yvln-ga/project2-remembook"
-            linkToWeb="https://project-remembook.herokuapp.com/"
-            image="./images/remembook.png"
+            title="Git Hired"
+            technology= "ReactJS, ExpressJS, NPM, PSQL, RESTful API"
+            description="With so many job search websites out there it can be hard to track things
+            like which jobs you’ve seen, when you applied, and who you’ve spoken to. GitHired aims
+            to solve this problem by giving users a centralized location for all of their job search
+            management needs."
+            linkToGit="https://github.com/rohara-gleveline"
+            linkToWeb="http://githiredpotoo.surge.sh/"
+            image="./images/githired.png"
           />
 
           <Project
             title="Meteo Fly High"
-            description="Nanani"
+            technology= "ReactJS, ExpressJS, NPM, PSQL, RESTful API"
+            description="Save your favorite cities in the world and know the weather in a glance!"
             linkToGit="https://git.generalassemb.ly/flyhigh"
             linkToWeb="https://git.generalassemb.ly/flyhigh"
             image="./images/flyhigh.png"
           />
 
           <Project
-            title="Git Hired"
-            description="Nanani"
-            linkToGit="https://github.com/rohara-gleveline"
-            linkToWeb="http://githiredpotoo.surge.sh/"
-            image="./images/githired.png"
+            title="Remembook"
+            technology= "HTML, CSS, JS, jQuery, NPM, ExpressJS, PassportJS, PSQL, RESTful API"
+            description="The 'anti-facebook' app. Not focus on your face but on your mind.
+            Not focus on others's life but on yours."
+            linkToGit="https://git.generalassemb.ly/yvln-ga/project2-remembook"
+            linkToWeb="https://project-remembook.herokuapp.com/"
+            image="./images/remembook.png"
+          />
+
+          <Project
+            title="BattleShip"
+            technology= "HTML, CSS, JS, jQuery"
+            description="Each player secretly arranges five ships on their primary grid :
+            A carrier, a battleship, a cruiser, a submarine and a destroyer.
+            Each ship occupies a number of consecutive squares on the grid."
+            linkToGit="https://git.generalassemb.ly/yvln-ga/project1-battleship"
+            linkToWeb="https://git.generalassemb.ly/pages/yvln-ga/project1-battleship/"
+            image="./images/battleship.png"
           />
 
         </div>
