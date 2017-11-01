3417128af9cefbdaf1325005b40b112a7df1a025
yvln <sayyveline@gmail.com>
2017-11-01 10:34:47 -0400

    update footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,7 +7,7 @@ class Footer extends Component {
       <div className="FooterContainer">
         <div className="Footer">
           <p>Made with <span className="heartfooter">&#9829;</span> by Yveline Say</p>
-          <p>Code available on github</p>
+          <p>Code available on <a className="githublinkfooter" href="https://github.com/yvln/mywebsite" target="_blank">github</a></p>
           <p>Copyright &#169; 2017 Yveline Say. All rights reserved.</p>
         </div>
       </div>
