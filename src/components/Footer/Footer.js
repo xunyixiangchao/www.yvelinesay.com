4f32ce4065168dbfe0d1d60e851504f2eb6c14a7
yvln <sayyveline@gmail.com>
2017-11-01 10:01:12 -0400

    created the footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,7 +6,9 @@ class Footer extends Component {
     return (
       <div className="FooterContainer">
         <div className="Footer">
-          FOOTER
+          <p>Made with <span className="heartfooter">&#9829;</span> by Yveline Say</p>
+          <p>Code available on github</p>
+          <p>Copyright &#169; 2017 Yveline Say. All rights reserved.</p>
         </div>
       </div>
     );
