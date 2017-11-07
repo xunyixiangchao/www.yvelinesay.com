b96a082124db35e68778d901bf4e83b4f814cd94
yvln <sayyveline@gmail.com>
2017-11-07 11:55:28 -0500

    update contact page

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -6,21 +6,35 @@ class Contact extends Component {
     return (
       <div id="ContactContainer">
         <div className="Contact">
-          <div className="LinkedIn">
-            <a href="https://www.linkedin.com/in/yveline-say/" rel="noopener noreferrer" target="_blank" ><img draggable="false" alt="linkedin icon" src="./images/linkedin.png" className="contactIcon" /></a>
+
+          <div className="HireMe">
+            <span className="HireMeEn">
+              Want to hire me?
+            </span>
+            <span className="HireMeFr">
+              Interessé(e) par mon profil?
+            </span>
           </div>
-          <div className="GitHub">
-            <a href="https://github.com/yvln" rel="noopener noreferrer" target="_blank" ><img draggable="false" alt="github icon" src="./images/github.png" className="contactIcon" /></a>
+
+          <span className="SayHello">Say Hello!</span>
+          <div className="ContactIconsContainer">
+            <div className="LinkedIn">
+              <a href="https://www.linkedin.com/in/yveline-say/" rel="noopener noreferrer" target="_blank" ><img draggable="false" alt="linkedin icon" src="./images/linkedin.png" className="contactIcon" /></a>
+            </div>
+            <div className="GitHub">
+              <a href="https://github.com/yvln" rel="noopener noreferrer" target="_blank" ><img draggable="false" alt="github icon" src="./images/github.png" className="contactIcon" /></a>
+            </div>
+            <div className="Email">
+              <a href="mailto:sayyveline@gmail.com" rel="noopener noreferrer" target="_blank" ><img draggable="false" alt="email icon" src="./images/email.png" className="contactIcon" /></a>
+            </div>
+            <div className="Twitter">
+              <a href="https://twitter.com/YvelineSay" rel="noopener noreferrer" target="_blank" ><img draggable="false" alt="twitter icon" src="./images/twitter.png" className="contactIcon" /></a>
+            </div>
+            <div className="Instagram">
+              <a href="https://www.instagram.com/xiaoolei/" rel="noopener noreferrer" target="_blank" ><img draggable="false" alt="instagram icon" src="./images/instagram.png" className="contactIcon" /></a>
+            </div>
           </div>
-          <div className="Email">
-            <a href="mailto:sayyveline@gmail.com" rel="noopener noreferrer" target="_blank" ><img draggable="false" alt="email icon" src="./images/email.png" className="contactIcon" /></a>
-          </div>
-          <div className="Twitter">
-            <a href="https://twitter.com/YvelineSay" rel="noopener noreferrer" target="_blank" ><img draggable="false" alt="twitter icon" src="./images/twitter.png" className="contactIcon" /></a>
-          </div>
-          <div className="Instagram">
-            <a href="https://www.instagram.com/xiaoolei/" rel="noopener noreferrer" target="_blank" ><img draggable="false" alt="instagram icon" src="./images/instagram.png" className="contactIcon" /></a>
-          </div>
+
         </div>
       </div>
     );
